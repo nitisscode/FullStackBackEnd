b8a1ace7f4614a268ca5d00e28900aebadde5478
nitisscode <nitishkumar50805@gmail.com>
2025-01-30 21:56:07 +0530

    cors updated

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,7 +1,7 @@
 const cors = require("cors");
 
 const corsOptions = {
-    origin: "http://localhost:5173", // Replace with your front-end URL
+    origin: ["http://localhost:5173","https://vite-react-mocha-iota.vercel.app/"], // Replace with your front-end URL
     methods: ["GET", "POST", "PUT","PATCH", "DELETE"], // Allowed HTTP methods
     allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
   };
